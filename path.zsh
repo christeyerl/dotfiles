4bad76a456a5a81ebc97dded5929e8c87ea0acc5
Chris Teyerl <44904418+christeyerl@users.noreply.github.com>
2025-09-01 12:58:17 +0200

    Fix issue

--- path.zsh
+++ path.zsh
@@ -3,11 +3,11 @@ export PATH="$HOME/Library/Application Support/Herd/bin/":$PATH
 export HERD_PHP_84_INI_SCAN_DIR="$HOME/Library/Application Support/Herd/config/php/84/"
 export HERD_PHP_83_INI_SCAN_DIR="$HOME/Library/Application Support/Herd/config/php/83/"
 
-[[ -f "/Applications/Herd.app/Contents/Resources/config/shell/zshrc.zsh" ]] && builtin source "/Applications/Herd.app/Contents/Resources/config/shell/zshrc.zsh"
-
 # Load NVM binaries
 export NVM_DIR="$HOME/Library/Application Support/Herd/config/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+[[ -f "/Applications/Herd.app/Contents/Resources/config/shell/zshrc.zsh" ]] && builtin source "/Applications/Herd.app/Contents/Resources/config/shell/zshrc.zsh"
 
 # Load Composer tools
 export PATH="$HOME/.composer/vendor/bin:$PATH"
